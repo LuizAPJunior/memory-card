6d197681f56be29df90b35ca3742229db09b6df8
LuizAPJunior <luiz.alpjr@gmail.com>
2022-11-22 00:58:31 -0300

    add grid center

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,42 +1,38 @@
-import { useState, useEffect} from 'react';
-import '../assets/styles/Card.css'
+import { useState, useEffect } from 'react';
+import '../assets/styles/Card.css';
 
 function Card(props) {
+	const [click, setClick] = useState(0);
 
-    const [click, setClick] = useState(0);
+	const handleClick = () => {
+		setClick(click + 1);
+	};
 
-    const handleClick = () => {
-        setClick(click + 1);
-    }
+	const reset = () => {
+		setClick(0);
+	};
 
-    const reset = () => {
-        setClick(0);
-    }
+	useEffect(() => {
+		if (click === 0) props.startGame();
+		if (click === 1) props.changeScore();
+		if (click > 1) {
+			props.gameOver();
+			reset();
+		}
+	}, [click]);
 
-    useEffect(() => {
-       
-        
-        if (click == 0) props.startGame();
-        if (click == 1) props.changeScore();
-        if (click > 1) {
-            props.gameOver();
-            reset();
-        }
-    }, [click]);
+	useEffect(() => {
+		if (props.reset === true) {
+			reset();
+		}
+	});
 
-    useEffect(()=>{
-        if (props.reset == true) {
-            reset();
-        }
-    })
-
-
-    return (
-        <div className='card' onClick={handleClick}>
-            <img src={props.image}></img>
-            <p>{props.name}</p>
-        </div>
-    );
+	return (
+		<div className="card" onClick={handleClick}>
+			<img src={props.image} alt={props.name}></img>
+			<p>{props.name}</p>
+		</div>
+	);
 }
 
 export default Card;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,3 @@
-
 import { useState } from 'react';
 
 import Card from './Card';
@@ -25,87 +24,91 @@ import imgKilljoy from '../assets/images/killjoy.png';
 import imgSage from '../assets/images/sage.png';
 import imgChamber from '../assets/images/chamber.png';
 
-
-
-
 function Game() {
+	const [score, setScore] = useState(0);
+	const [bestScore, setBestScore] = useState(score);
+	const [reset, setReset] = useState(false);
 
-    const [score, setScore] = useState(0);
-    const [bestScore, setBestScore] = useState(score);
-    const [reset, setReset] = useState(false);
+	const changeScore = () => {
+		setScore(score + 1);
+	};
 
-    const changeScore = () => {
-        setScore(score + 1);
-    }
+	const gameOver = () => {
+		console.log('game over');
+		if (score > bestScore) setBestScore(score);
+		setScore(0);
+		setReset(true);
+	};
 
-    const gameOver = () => {
-        console.log('game over');
-        if (score > bestScore) setBestScore(score);
-        setScore(0);
-        setReset(true);
+	const startGame = () => {
+		setReset(false);
+	};
 
-    }
+	let cardsArr = [
+		{ name: 'Jett', image: imgJett },
+		{ name: 'Raze', image: imgRaze },
+		{ name: 'Yoru', image: imgYoru },
+		{ name: 'Reyna', image: imgReyna },
+		{ name: 'Phoenix', image: imgPhoenix },
+		{ name: 'Neon', image: imgNeon },
+		{ name: 'Sova', image: imgSova },
+		{ name: 'Breach', image: imgBreach },
+		{ name: 'Skye', image: imgSkye },
+		{ name: 'Kayo', image: imgKayo },
+		{ name: 'Astra', image: imgAstra },
+		{ name: 'Viper', image: imgViper },
+		{ name: 'Brim', image: imgBrimstone },
+		{ name: 'Omen', image: imgOmen },
+		{ name: 'Cypher', image: imgCypher },
+		{ name: 'Killjoy', image: imgKilljoy },
+		{ name: 'Sage', image: imgSage },
+		{ name: 'Chamber', image: imgChamber },
+	];
 
-    const startGame = () => {
-        setReset(false);
-        console.log("começou");
-    }
+	const shuffleCards = (array) => {
+		let currentIndex = array.length,
+			randomIndex;
+		while (currentIndex !== 0) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex--;
+			[array[currentIndex], array[randomIndex]] = [
+				array[randomIndex],
+				array[currentIndex],
+			];
+		}
 
-    let cardsArr = [
-        { name: "Jett", image: imgJett },
-        { name: "Raze", image: imgRaze },
-        { name: "Yoru", image: imgYoru },
-        { name: "Reyna", image: imgReyna },
-        { name: "Phoenix", image: imgPhoenix },
-        { name: "Neon", image: imgNeon },
-        { name: "Sova", image: imgSova },
-        { name: "Breach", image: imgBreach },
-        { name: "Skye", image: imgSkye },
-        { name: "Kayo", image: imgKayo },
-        { name: "Astra", image: imgAstra },
-        { name: "Viper", image: imgViper },
-        { name: "Brim", image: imgBrimstone },
-        { name: "Omen", image: imgOmen },
-        { name: "Cypher", image: imgCypher },
-        { name: "Killjoy", image: imgKilljoy },
-        { name: "Sage", image: imgSage },
-        { name: "Chamber", image: imgChamber },
-    ];
+		return array;
+	};
 
+	cardsArr = shuffleCards(cardsArr);
 
-
-    const shuffleCards = (array) => {
-        let currentIndex = array.length, randomIndex;
-        while (currentIndex != 0) {
-            randomIndex = Math.floor(Math.random() * currentIndex);
-            currentIndex--;
-            [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-        }
-
-        return array;
-    }
-
-    cardsArr = shuffleCards(cardsArr);
-
-    return (
-        <div>
-            <header className="header">
-                <div>
-                    <h1>VALORANT</h1>
-                    <h2>memory card game</h2>
-                </div>
-                <div className='score'>
-                    <Score text={"Score: "} score={score} />
-                    <Score text={"Best Score: "} score={bestScore} />
-                </div>
-            </header>
-            <main className="cards">
-                {cardsArr.map(({ name, image }) => (
-                    <Card key={name} name={name} image={image} changeScore={changeScore} gameOver={gameOver} reset={reset} startGame={startGame} />
-                ))}
-            </main>
-        </div>
-    );
-};
+	return (
+		<div>
+			<header className="header">
+				<div>
+					<h1>VALORANT</h1>
+					<h2>memory card game</h2>
+				</div>
+				<div className="score">
+					<Score text={'Score: '} score={score} />
+					<Score text={'Best Score: '} score={bestScore} />
+				</div>
+			</header>
+			<main className="cards">
+				{cardsArr.map(({ name, image }) => (
+					<Card
+						key={name}
+						name={name}
+						image={image}
+						changeScore={changeScore}
+						gameOver={gameOver}
+						reset={reset}
+						startGame={startGame}
+					/>
+				))}
+			</main>
+		</div>
+	);
+}
 
 export default Game;
